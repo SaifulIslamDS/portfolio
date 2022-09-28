276794bbb21b4f09c4bd07bb59d4f8196e2da062
Saiful Islam <saif.shuvo@hotmail.com>
2022-09-28 11:39:13 +0600

    update

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App text-center">
-      <h1 className='text-6xl font-bold text-blue-900'>Saiful Shuvo</h1>
+      <h1 className='text-6xl font-black text-green-700'>Saiful Shuvo</h1>
       <h3 className=' text-3xl'>We build your digital presence.</h3>
       <p>Your website is your online identity. Make sure to make it lucrative.</p>
     </div>
