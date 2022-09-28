4454789cb0d0d73c1e07ea77555bece1bd364548
Saiful Islam <saif.shuvo@hotmail.com>
2022-09-28 11:10:11 +0600

    Tailwind css added

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Portfolio of Saiful Shuvo developed by react.</h1>
+      <h1 className='text-6xl font-bold text-blue-900'>Saiful Shuvo</h1>
+      <h3 className=' text-3xl'>We build your digital presence.</h3>
+      <p>Your website is your online identity. Make sure to make it lucrative.</p>
     </div>
   );
 }
