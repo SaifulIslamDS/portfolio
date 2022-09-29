6c9a8488073006ba98af72f398e1696cb639cbb8
Saiful Shuvo <saif.shuvo@hotmail.com>
2022-09-29 23:05:56 +0600

    Update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import './App.css';
+import Home from './Components/Home/home';
 
 function App() {
   return (
-    <div className="App text-center">
-      <h1 className='text-6xl font-black text-green-700'>Saiful Shuvo</h1>
-      <h3 className=' text-3xl'>We build your digital presence.</h3>
-      <p>Your website is your online identity. Make sure to make it lucrative.</p>
-    </div>
+    <>
+      <Home />
+    </>
   );
 }
 
---
+++ src/Components/Home/home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const home = () => {
+    return (
+        <div className='text-center bg-indigo-300 py-24'>
+            <h1 className='text-6xl font-black text-green-700'>Saiful Shuvo</h1>
+            <h3 className=' text-3xl'>We build your digital presence.</h3>
+            <p>Your website is your online identity. Make sure to make it lucrative.</p>
+        </div>
+    );
+};
+
+export default home;
